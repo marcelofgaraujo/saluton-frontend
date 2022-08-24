a9959f1d7f1c6e4fd1fa9300ae7389cd805035ba
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-08-24 16:16:58 -0300

    typed API response

---
+++ src/model/country.ts
@@ -0,0 +1,10 @@
+export type Country = {
+    Name: string,
+    Language: string,
+    Percent_Speak_English: string,
+    First_or_last_name: string,
+    Formal_or_informal: string,
+    Punctuality: string,
+    Time_Zone: string,
+    Insights_into_cultural_aspects: string
+}
