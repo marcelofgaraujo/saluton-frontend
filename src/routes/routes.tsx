63586a0ea5bf7d6c7e896a970657ff1b671b0c81
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-08-24 16:15:59 -0300

    affix routes to the project

---
+++ src/routes/routes.tsx
@@ -0,0 +1,10 @@
+import { useRoutes } from "react-router-dom";
+import Home from '../pages/home/home'
+import CountryPage from '../pages/countryPage/countryPage'
+
+export const RouteList = () => {
+    return useRoutes([
+        {path:'/', element:<Home/>},
+        {path:'/:Name', element: <CountryPage/>}
+    ])
+}
