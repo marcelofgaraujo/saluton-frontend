91da88f94114f5e3ba0aac9ed0e3a86f32706d1f
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-08-31 17:34:55 -0300

    greetings filter for avoiding error 404

--- src/pages/countryPage/countryPage.tsx
+++ src/pages/countryPage/countryPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams, useSearchParams } from 'react-router-dom';
 import { Country } from '../../types/country'
 import { Greeting } from '../../types/greeting';
@@ -8,13 +8,29 @@ import './style.css';
 function CountryPage() {
     const [country, setCountry] = useState<Country>();
     const [greetings, setGreetings] = useState<Greeting[]>();
-    const [allGreetings, setAllGreetings] = useState<Greeting[]>();
+    const [firstName, setFirstName] = useState<string>();
+    const [lastName, setLastName] = useState<string>();
+    const [clicked, setClicked] = useState<boolean>(false);
+
     // const [languageParams, setLanguageParams] = useSearchParams();
+
     const { Name, Language } = useParams<string>();
 
     useEffect(() => {
-        loadCountryInfo(), getAllGreetings() /*loadGreeting()*/
+        loadCountryInfo(), loadGreeting()
     }, [])
+    
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        let name = e.target.value.split(" ")
+        setFirstName(name[0])
+        name.length > 1 && setLastName(name[1])
+    }
+
+    const handleNameReplace = (string: string) => {
+        string.replace('[first name]', firstName!)
+        lastName && string.replace('[last name]', lastName)
+        return string
+    }
 
     // const getLanguageFilter = () => {
     //     languageParams.get('language')
@@ -27,33 +43,26 @@ function CountryPage() {
         setCountry(countryResponse.data)
     }
 
-    const getAllGreetings = async () => {
-        const fetchAllGreetings = await axios.get<Greeting[]>(`http://localhost:3000/greetings`)
-        setAllGreetings(fetchAllGreetings.data)
-        loadGreeting();
-    }
-
     const loadGreeting = async () => {
-
+        // tratamento de dados - array de linguagens do país
         if (!Language) return console.error('foi possivel nao boy')
         const langGreetings = Language.replace(/,/g, '').split(" ")
         if (!langGreetings) return console.error('foi possivel nao boy')
         const andIndex = langGreetings.findIndex(item => item === 'and')
         andIndex > -1 && langGreetings.splice(andIndex, 1)
 
-        console.log(langGreetings)
+        // get em todos os greetings existentes
+        const ALL_GREETINGS = (await axios.get<Greeting[]>(`http://localhost:3000/greetings`)).data
 
-        let existentGreetings: string[] = []
-        if (!allGreetings) return console.error('deu nao boy')
-        allGreetings.forEach(item => existentGreetings.push(item.Language))
-        console.log(existentGreetings)
-
-        let fetchGreetings: Greeting[] = [];
+        // comparaçao entre as linguages do país e as linguagens de greetings existentes, dando get nos que existem
+        let existentGreetings: Greeting[] = [];
         for (let language of langGreetings) {
-            let mockGreeting = await axios.get<Greeting>(`http://localhost:3000/greetingquery/${language}`);
-            //to-do: FETCH EM TODOS OS GREETINGS E DEPOIS FILTRAR GREETINGS EXISTENTES
+            if (ALL_GREETINGS.some((item) => item.Language === language)) {
+                let fetchGreeting = await axios.get<Greeting>(`http://localhost:3000/greetingquery/${language}`);
+                existentGreetings.push(fetchGreeting.data)
+            }
         }
-        setGreetings(fetchGreetings)
+        setGreetings(existentGreetings)
     }
 
     return (
@@ -68,6 +77,25 @@ function CountryPage() {
                 <li><b>Time Zone:</b> {country && country.Time_Zone}</li>
                 <li><b>Cultural aspects:</b> {country && country.Insights_into_cultural_aspects}</li>
             </ul>
+            {greetings && greetings.length < 1 ? <p>sorry, we don't find greetings for this country :[ (yet...)</p> :
+                <>
+                    <label htmlFor='name'>Name:</label>
+                    <input name='name' onChange={(e) => handleName(e)} placeholder='type first and last name' type={'text'} />
+                    <button onClick={() => setClicked(true)}>Generate greetings</button>
+                    {clicked && 
+                        <>
+                            {
+                                greetings && greetings.map((item, index) => {
+                                    <div key={index}>
+                                        <li><b>Language:</b> {item.Language}</li>
+                                        <li><b>Men:</b> {handleNameReplace(item.Greeting_Men)}</li>
+                                        <li><b>Women:</b> {handleNameReplace(item.Greeting_Women)}</li>
+                                        <li><b>Dear Sir or Madam:</b> {handleNameReplace(item.Dear_Sir_or_Madam)}</li>
+                                    </div>
+                                })
+                            }
+                        </>}
+                </>}
         </div>
     )
 }
