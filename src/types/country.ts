5e44e13388256c72e16179d575e2b294e396a716
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-09-02 18:05:17 -0300

    limited strings for two properties of countries

--- src/types/country.ts
+++ src/types/country.ts
@@ -2,8 +2,8 @@ export type Country = {
     Name: string,
     Language: string,
     Percent_Speak_English: string,
-    First_or_last_name: string,
-    Formal_or_informal: string,
+    First_or_last_name: 'First' | 'Last',
+    Formal_or_informal: 'Formal' | 'Informal',
     Punctuality: string,
     Time_Zone: string,
     Insights_into_cultural_aspects: string
