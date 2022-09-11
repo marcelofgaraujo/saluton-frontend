2eb3e4f950746249fb51bd7cc987c53dd4dc0f31
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-09-11 14:19:02 -0300

    added loading screen in home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,17 +6,22 @@ import './style.css'
 
 function Home() {
     const [countries, setCountries] = useState<Country[]>();
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
         loadData()
     }, [])
 
     const loadData = async () => {
+        setLoading(true)
         let response = await api.getAllCountries()
         setCountries(response)
+        setLoading(false)
     }
 
     return (
+        
+        loading ? <h3>ŝarĝo...</h3> :
         <div className="homeBody">
             <h1 className='title'>Saluton</h1>
             {countries && countries.map((item, index) =>
