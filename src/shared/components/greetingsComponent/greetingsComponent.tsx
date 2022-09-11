575a4756cd0cfb611cb0b5247fc69f8a1a6c633c
marcelofgaraujo <marcelofgaraujo@gmail.com>
2022-09-11 14:19:25 -0300

    feature: copy button for every greeting

--- src/shared/components/greetingsComponent/greetingsComponent.tsx
+++ src/shared/components/greetingsComponent/greetingsComponent.tsx
@@ -1,21 +1,38 @@
-import { Greeting } from "../../../types/greeting"
-import './style.css'
+import { Greeting } from "../../../types/greeting";
+import copy from "copy-to-clipboard";
+import './style.css';
 
 type Props = {
     data: Greeting,
     replaceName: (string: string) => string
 }
 
-export const GreetingsComponent = ({data, replaceName}: Props) => {
+const handleCopy = (string: string) => {
+    copy(string)
+    return alert('Greeting sucessfully copied to clipboard!')
+}
+
+export const GreetingsComponent = ({ data, replaceName }: Props) => {
 
     return (
+
         <div>
             <h2>{data.Language} greetings</h2>
-            <ul>
-                <li><b>Men:</b> {replaceName(data.Greeting_Men)}</li>
-                <li><b>Women:</b> {replaceName(data.Greeting_Women)}</li>
-                <li><b>Dear Sir or Madam:</b> {replaceName(data.Dear_Sir_or_Madam)}</li>
+            <ul className="list-greetings">
+                <li>
+                    <b>Men:</b> {replaceName(data.Greeting_Men)}
+                    <div className="copy" onClick={() => handleCopy(replaceName(data.Greeting_Men))} title={'Copy to clipboard'} />
+                </li>
+                <li>
+                    <b>Women:</b> {replaceName(data.Greeting_Women)}
+                    <div className="copy" onClick={() => handleCopy(replaceName(data.Greeting_Women))} title={'Copy to clipboard'} />
+                </li>
+                <li>
+                    <b>Dear Sir or Madam:</b> {replaceName(data.Dear_Sir_or_Madam)}
+                    <div className="copy" onClick={() => handleCopy(replaceName(data.Dear_Sir_or_Madam))} title={'Copy to clipboard'} />
+                </li>
             </ul>
         </div>
+
     )
 }
